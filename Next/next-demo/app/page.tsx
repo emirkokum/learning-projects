39bf7601f4dee07f72c9c0f52f5e16bce86956e2
Emir Kokum <emir_kokum_@hotmail.com>
2025-10-28 14:47:55 +0300

    add next project

---
+++ Next/next-demo/app/page.tsx
@@ -0,0 +1,14 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
+const Page = () => {
+    return (
+
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br/>Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackhathons, meetups, conferences, All in one place</p>
+
+            <ExploreBtn/>
+        </section>
+    )
+}
+export default Page
---
+++ Next/next-demo/components/ExploreBtn.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const ExploreBtn = () => {
+    return (
+        <button type="button" onClick={() => console.log('Button has Clicked')}>ExploreBtn</button>
+    )
+}
+
+export default ExploreBtn
